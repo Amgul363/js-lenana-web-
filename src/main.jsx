a784e5eead40255f895652cbed883749a19ed61c
Amgul363 <angelbetty608@gmail.com>
2025-08-27 16:46:40 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename='"/js-lenana-web"'>
+    <BrowserRouter basename="/js-lenana-web">
       <App />
     </BrowserRouter>
 
